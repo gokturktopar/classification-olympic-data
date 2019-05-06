ea62df24d6175984d5aef3d1d979961b8f64f711
Göktürk Topar <gokturktopar@gmail.com>
2019-05-07 02:50:56 +0300

    Update code.R

--- code.R
+++ code.R
@@ -1,11 +1,10 @@
-#set working dir
-setwd("C:/Users/pico/Desktop/gokturk/4.sýnýf/machineLearning/proje/120-years-of-olympic-history-athletes-and-results (1)")
+
 #read Data
 source("handleMissingValues.R")
 olympicdata<-read.table("athlete_events.csv", header = TRUE, sep = ",", dec = ",")
+#Handle Missin Values
 olympicdata$Height=handleMissingValues(olympicdata$Height)
-View(olympicdata$Weight)
 olympicdata$Weight<-as.numeric(levels(olympicdata$Weight))[olympicdata$Weight]
 olympicdata$Weight=handleMissingValues(olympicdata$Weight)
 View(olympicdata)
-View(olympicdata$Weight)
+
