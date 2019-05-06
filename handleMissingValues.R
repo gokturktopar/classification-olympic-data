3bb7199169836c87b1a546bc309f1d04f571b046
Göktürk Topar <gokturktopar@gmail.com>
2019-05-07 02:48:01 +0300

    first commit

---
+++ handleMissingValues.R
@@ -0,0 +1,8 @@
+handleMissingValues <- function(data) {
+  nonNAIndex=which(!is.na(data))
+  sumofNonNa=sum(data[nonNAIndex])
+  meanofNonNa=sumofNonNa/length(nonNAIndex)
+  NAIndex=which(is.na(data))
+  data[NAIndex]<-meanofNonNa
+  return(data) 
+}
